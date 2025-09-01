f75dbcdea2b5f5ef4bb26d052a46148978518c70
Flaviano Rodrigues dos Santos <flavianorodrigues147@gmail.com>
2025-09-01 14:35:43 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,12 @@
+num1 = float(input("Digite o primeiro número: "))
+num2 = float(input("Digite o segundo número: "))
+num3 = float(input("Digite o terceiro número: "))
+num4 = float(input("Digite o quarto número: "))
+media = (num1 + num2 + num3 + num4) / 4
+print("A média dos números é:", media)
+
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+num3 = float(input("Enter third number:"))
+media = (num1+num2+num3)/3
+print("Media = %.2f" % (media))
