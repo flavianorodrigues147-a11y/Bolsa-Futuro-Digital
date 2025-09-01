57522e89e0b3d05548faa476dad242efe74df2ea
Flaviano Rodrigues dos Santos <flavianorodrigues147@gmail.com>
2025-09-01 20:41:56 -0300

    Calcular área de um quadrado.

---
+++ quadrado.py
@@ -0,0 +1,8 @@
+# area do quadrado e volume do quadrado
+l = float(input("Digite o valor do lado do quadrado: "))
+area = l * l  # lado vezes lado
+print("A área do quadrado é:", area)
+# volume do quadrado
+l = float(input("Digite o valor do lado do quadrado para calcular o volume: "))
+volume = l ** 3  # cumprimento do lado elevado ao cubo
+print("O volume do quadrado é:", volume)
