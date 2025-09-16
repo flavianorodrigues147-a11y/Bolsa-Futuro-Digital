5e6d3b62ab260da124720aac2c02ad992723fe1e
Flaviano Rodrigues dos Santos <flavianorodrigues147@gmail.com>
2025-09-16 18:58:52 -0300

    math

---
+++ biblioteca04.py
@@ -0,0 +1,5 @@
+#Com math, calcule a potência de 2 elevado a 10.
+
+import math as m #importa a biblioteca math e renomeia ela como m para facilitar o uso
+potencia = m.pow(2, 10) #pow é a função que calcula a potência
+print(f'2 elevado a 10 é igual a: {potencia}')
