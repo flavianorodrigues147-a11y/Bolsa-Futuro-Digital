d96e16f321982da824565168b073ae474ed06781
Flaviano Rodrigues dos Santos <flavianorodrigues147@gmail.com>
2025-09-16 18:58:19 -0300

    datetime

--- biblioteca03.py
+++ biblioteca03.py
@@ -1,4 +1,8 @@
 # Use a biblioteca datetime para mostrar a data e hora atuais.
 import datetime as dt
-hora_agora = dt.datetime.now()
+hora_agora = dt.datetime.now() #now() pega a data e hora atuais
 print(f'Dia e hora atuais: {hora_agora}')
+
+#datetime é uma biblioteca padrão do Python que fornece classes para manipulação de datas e horas.
+#No exemplo acima, usamos a função now() da classe datetime para obter a data e hora atuais.
+#A biblioteca datetime é muito útil para trabalhar com datas e horas em aplicações que precisam lidar com agendamentos, registros de eventos, entre outros.
